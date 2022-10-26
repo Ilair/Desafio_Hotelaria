fd371346271ee0a3b75a51bc66c3b69af5e4b23a
Ilair dos Santos <ilair30@gmail.com>
2022-10-25 23:58:22 -0300

    Ajustes2

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -24,7 +24,7 @@ namespace DesafioProjetoHospedagem.Models
             else
             {
                 // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
-                Console.WriteLine("Não há mais quartos disponíveis!");
+                throw new Exception("Capacidade da suíte excedida!");
             }
         }
 
